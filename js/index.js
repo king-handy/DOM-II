b372e2be3add145c3e94602ac1f2fd9abf9830e5
king-handy <a.king.life@pm.me>
2021-06-06 15:44:13 -0400

    2 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+// * [ ] Using your [index.js file](js/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some event types you could try to use:
+//   * `mouseover`
+//   * `keydown`
+//   * `wheel`
+//   * `load`
+//   * `focus`
+//   * `resize`
+//   * `scroll`
+//   * `select`
+//   * `dblclick`
+//   * `drag / drop`
+
+const mouseOver = document.getElementsByClassName('nav-link')
+mouseOver.addEventListener('mouseover', event => {
+    alert(`Mouse over link!`)
+})
+
+window.addEventListener('load', event => {
+    console.log(`Page loaded.`)
+})
+
